547674666490cff5d97d711df538391664714da8
Artur Shkred <s15444@pjwstk.edu.pl>
2020-03-09 04:15:42 +0100

    configured React Router

--- event-frontend/src/App.js
+++ event-frontend/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// Routes
+import Home from 'screens/shared/pages/Home';
 
 const App = () => {
-  return <div className="App">Event</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact render={() => <Home />} />
+        <Route path="/home" exact render={() => <Home />} />
+        <Route render={() => <h1>NotFound</h1>} />
+      </Switch>
+    </div>
+  );
 };
 
-export default App;
+const Event = props => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+export default Event;
---
+++ event-frontend/src/screens/shared/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="home-page">
+      <h1>Event</h1>
+    </div>
+  );
+};
