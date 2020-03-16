003be1c962714a1530c2900d58c1cf5f3445ea86
Artur Shkred <s15444@pjwstk.edu.pl>
2020-03-16 02:36:43 +0100

    added types for withSuspense (High Order Component)

--- event-frontend/src/hoc/withSuspense.tsx
+++ event-frontend/src/hoc/withSuspense.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Preloader from 'screens/shared/components/common/Preloader';
 
 // High Order Component (hoc)
-export const withSuspense = Component => {
-  return props => {
+export const withSuspense = <P extends object>(
+  Component: React.ComponentType<P>
+) => {
+  return (
+    props: JSX.IntrinsicAttributes & P & { children?: React.ReactNode }
+  ) => {
     return (
       <React.Suspense fallback={<Preloader />}>
         <Component {...props} />;
